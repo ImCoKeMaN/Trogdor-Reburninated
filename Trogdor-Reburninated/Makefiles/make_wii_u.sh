2bbe0826e44898d2b994473ad8961885010b0318
Mode8fx <gateguy33@gmail.com>
2026-05-16 19:07:32 -0400

    Made more sounds static
    The Gamecube's limited RAM is making this complicated. I could fall back to lower-quality audio, but I don't want to. I wish SDL_mixer just let you use different sample sizes for music and sound effects...
    
    Also, since preloading sounds is now the default, I removed that compiler flag and added an inverted flag called LIMITED_PRELOAD (only intended for systems that need to save RAM)

--- Trogdor-Reburninated/Makefiles/make_wii_u.sh
+++ Trogdor-Reburninated/Makefiles/make_wii_u.sh
@@ -3,12 +3,12 @@ export DEVKITPRO=/opt/devkitpro
 rm -r build_wii_u
 mkdir build_wii_u
 cd build_wii_u
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=$DEVKITPRO/wut/share/wut.toolchain.cmake -DWIIU=ON -DNINTENDO_LAYOUT=ON -DPRELOAD_SOUNDS=ON
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=$DEVKITPRO/wut/share/wut.toolchain.cmake -DWIIU=ON -DNINTENDO_LAYOUT=ON
 make
 
 cd ../
 rm -r build_wii_u_whub
 mkdir build_wii_u_whub
 cd build_wii_u_whub
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=$DEVKITPRO/wut/share/wut.toolchain.cmake -DWIIU=ON -DNINTENDO_LAYOUT=ON -DPRELOAD_SOUNDS=ON -DNO_QUIT=ON
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=$DEVKITPRO/wut/share/wut.toolchain.cmake -DWIIU=ON -DNINTENDO_LAYOUT=ON -DNO_QUIT=ON
 make
